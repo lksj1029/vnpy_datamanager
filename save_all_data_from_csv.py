e0f012122283a2986bcc2e7e00b3e5b06824cfc1
lksj1029 <liu.kai81@gmail.com>
2023-12-20 15:52:34 +0800

    [MOD] 获取日K线数据并写入VNPY数据库

--- save_all_data_from_csv.py
+++ save_all_data_from_csv.py
@@ -2,35 +2,39 @@ import os
 from tqdm import tqdm
 
 from engine import ManagerEngine
+from akshare_kdata import akshare_kdata
 from vnpy.trader.constant import Interval, Exchange
 from vnpy.trader.engine import MainEngine, EventEngine
 
 
-filedir: str = 'kdata_daily'
-filenames: str = os.listdir(filedir)
-me = ManagerEngine(MainEngine, EventEngine)
-for file in tqdm(filenames, position=0, desc='导入K线数据'):
-    filepath: str = filedir + '\\' + file
-    symbol: str = file.split('.')[1]
-    if 'sh' in file:
-        exchange: Exchange = Exchange.SSE
-    else:
-        exchange: Exchange = Exchange.SZSE
-    me.import_data_from_csv(
-        file_path=filepath,
-        symbol=symbol,
-        exchange=exchange,
-        interval=Interval.DAILY,
-        tz_name='Asia/Shanghai',
-        datetime_head='datetime',
-        open_head='open',
-        high_head='high',
-        low_head='low',
-        close_head='close',
-        volume_head='volume',
-        turnover_head='turnover',
-        open_interest_head='',
-        amplitude_head='amplitude',
-        datetime_format='%Y-%m-%d'
-
-    )
+"""获取日K线数据并且写入VNPY数据库"""
+if __name__ == '__main__':
+    aks = akshare_kdata()
+    aks.daily_kdatafeed()
+    filedir: str = 'kdata_daily'
+    filenames: str = os.listdir(filedir)
+    me = ManagerEngine(MainEngine, EventEngine)
+    for file in tqdm(filenames, position=0, desc='导入K线数据'):
+        filepath: str = filedir + '\\' + file
+        symbol: str = file.split('.')[1]
+        if 'sh' in file:
+            exchange: Exchange = Exchange.SSE
+        else:
+            exchange: Exchange = Exchange.SZSE
+        me.import_data_from_csv(
+            file_path=filepath,
+            symbol=symbol,
+            exchange=exchange,
+            interval=Interval.DAILY,
+            tz_name='Asia/Shanghai',
+            datetime_head='datetime',
+            open_head='open',
+            high_head='high',
+            low_head='low',
+            close_head='close',
+            volume_head='volume',
+            turnover_head='turnover',
+            open_interest_head='',
+            amplitude_head='amplitude',
+            datetime_format='%Y-%m-%d'
+        )
