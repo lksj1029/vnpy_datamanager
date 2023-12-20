44d6c04ca93e674bc6d74d49bb877c4dbf930063
lksj1029 <liu.kai81@gmail.com>
2023-12-20 15:10:30 +0800

    [MOD] 一次性导入所有数据

--- save_all_data_from_csv.py
+++ save_all_data_from_csv.py
@@ -1,25 +1,36 @@
+import os
+from tqdm import tqdm
+
 from engine import ManagerEngine
 from vnpy.trader.constant import Interval, Exchange
-from vnpy.trader.engine import BaseEngine, MainEngine, EventEngine
+from vnpy.trader.engine import MainEngine, EventEngine
 
 
+filedir: str = 'kdata_daily'
+filenames: str = os.listdir(filedir)
 me = ManagerEngine(MainEngine, EventEngine)
-filepath: str = 'kdata_daily\\000001.csv'
-me.import_data_from_csv(
-    file_path=filepath,
-    symbol='000001',
-    exchange=Exchange.SSE,
-    interval=Interval.DAILY,
-    tz_name='Asia/Shanghai',
-    datetime_head='datetime',
-    open_head='open',
-    high_head='high',
-    low_head='low',
-    close_head='close',
-    volume_head='volume',
-    turnover_head='turnover',
-    open_interest_head='',
-    amplitude_head='amplitude',
-    datetime_format='%Y-%m-%d'
+for file in tqdm(filenames, position=0, desc='导入K线数据'):
+    filepath: str = filedir + '\\' + file
+    symbol: str = file.split('.')[1]
+    if 'sh' in file:
+        exchange: Exchange = Exchange.SSE
+    else:
+        exchange: Exchange = Exchange.SZSE
+    me.import_data_from_csv(
+        file_path=filepath,
+        symbol=symbol,
+        exchange=exchange,
+        interval=Interval.DAILY,
+        tz_name='Asia/Shanghai',
+        datetime_head='datetime',
+        open_head='open',
+        high_head='high',
+        low_head='low',
+        close_head='close',
+        volume_head='volume',
+        turnover_head='turnover',
+        open_interest_head='',
+        amplitude_head='amplitude',
+        datetime_format='%Y-%m-%d'
 
-)
+    )
